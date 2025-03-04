abcf946faaf9e6bfe857105f70b6b77c5ad0be07
naysu06 <franzdominicc@gmail.com>
2025-03-04 18:39:56 +0800

    added tailwind to items/create

--- Simple-Routing-to-Views/resources/views/item.blade.php
+++ Simple-Routing-to-Views/resources/views/item.blade.php
@@ -2,25 +2,28 @@
 <html>
     <head>
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        @vite('resources/css/app.css')
     </head>
-    <body>
-        <h1>hello world</h1>
-        
-        <a href="{{ url('/items/create') }}" class="btn btn-primary">Add New Item</a>
-        <ul>
+    <body class="m-1">
+        <div class="main-wrap bg-slate-500 max-w-[400px] rounded-md p-5">
+            <h1 class="text-3xl font-bold">Hello world</h1>
+            
+            <a href="{{ url('/items/create') }}" class="btn btn-primary bg-blue-800 text-white p-1 rounded-md">Add New Item</a>
+            <ul>
 
-        @foreach($items as $item)
-            <li>{{ $item->name }} - {{ $item->description }} <a href="{{ route('items.show', $item->id) }}">View Item</a>;</li>
-        @endforeach
+            @foreach($items as $item)
+                <li class="text-white">{{ $item->name }} - {{ $item->description }} <a href="{{ route('items.show', $item->id) }}" class="text-white bg-blue-800 p-1 rounded-md">View Item</a></li>
+            @endforeach
 
-        </ul>
+            </ul>
 
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-        
-        <script src="{{ asset('js/app.js') }}"></script>
+            @if(session('success'))
+                <div class="alert alert-success text-white">
+                    {{ session('success') }}
+                </div>
+            @endif
+            
+            <script src="{{ asset('js/app.js') }}"></script>
+        </div>
     </body>
 </html>
